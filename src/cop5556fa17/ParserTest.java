40d78e6ed847a1e229ff90e40b9e4db661fad85e
Sreeharsha Poluru <sreeharshapoluru@gmail.com>
2017-10-12 22:12:10 -0400

    after final testing assign - 3

--- src/cop5556fa17/ParserTest.java
+++ src/cop5556fa17/ParserTest.java
@@ -44,7 +44,7 @@ public class ParserTest {
 		Parser parser = new Parser(scanner); //Create a parser
 		thrown.expect(SyntaxException.class);
 		try {
-			ASTNode ast = parser.parse(); //Parse the program, which should throw an exception
+			ASTNode ast = parser.parse();; //Parse the program, which should throw an exception
 		} catch (SyntaxException e) {
 			show(e);  //catch the exception and show it
 			throw e;  //rethrow for Junit
@@ -82,172 +82,11 @@ public class ParserTest {
 		assertEquals("k", dec.name);
 		assertNull(dec.e);
 	}
+
 	
 	@Test
 	public void testcase1() throws SyntaxException, LexicalException {
-		String input = "2";
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		} 
-	}
-	@Test
-	public void testcase2() throws SyntaxException, LexicalException {
-		String input = "a bcd";
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		} 
-	}
-	
-	@Test
-	public void testcase3() throws SyntaxException, LexicalException {
-		String input = "cart_x cart_y";
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		} 
-	}
-	
-	@Test
-	public void testcase4() throws SyntaxException, LexicalException {
-		String input = "prog int 2";
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		} 
-	}
-	
-	@Test
-	public void testcase5() throws SyntaxException, LexicalException {
-		String input = "prog image[filepng,png] imageName <- imagepng"; //Error as there is not semicolon for ending the statement
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		//Parser.program();
-		thrown.expect(SyntaxException.class);
-		try {
-			parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		} 
-	}
-	
-	@Test
-	public void testcase6() throws SyntaxException, LexicalException {
-		String input = "imageDeclaration image[\"abcd\"] "; //Should fail for image[""]
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		} 
-	}
-	
-	/*@Test
-	public void testcase7() throws SyntaxException, LexicalException {
-		String input = "prog image[filepng,png] imageName <- imagepng; \n boolean ab=true;"; 
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		Program ast=parser.program();
-		show(ast);
-		assertEquals("prog",ast.name);
-		// First Declaration statement
-		Declaration_Image dec = (Declaration_Image) ast.decsAndStatements.get(0);  
-		assertEquals(KW_image, dec.firstToken.kind);
-		assertEquals("imageName", dec.name);
-		Expression_Ident ei=(Expression_Ident)dec.xSize;
-		assertEquals("filepng",ei.name);
-		ei=(Expression_Ident)dec.ySize;
-		assertEquals("png",ei.name);
-		Source_Ident s=(Source_Ident) dec.source;
-	    assertEquals("imagepng",s.name);
-		// Second Declaration statement
-	    Declaration_Variable dec2 = (Declaration_Variable) ast.decsAndStatements.get(1);  
-		assertEquals("ab", dec2.name);
-		assertEquals(KW_boolean, dec2.firstToken.kind);
-		Expression_BooleanLit ebi=(Expression_BooleanLit)dec2.e;
-		assertEquals(true,ebi.value);		
-	}
-	
-	@Test
-	public void testcase8() throws SyntaxException, LexicalException {
-		String input = "prog image[filepng,jpg] imageName;"; 
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		Program ast=parser.program();
-		show(ast);
-		assertEquals("prog",ast.name);
-		Declaration_Image dec1 = (Declaration_Image) ast.decsAndStatements.get(0); 
-		assertEquals(dec1.name,"imageName");
-		Expression_Ident exi=(Expression_Ident)dec1.xSize;
-		Expression_Ident eyi=(Expression_Ident)dec1.ySize;
-		assertEquals(exi.name,"filepng");
-		assertEquals(eyi.name,"jpg");
-		assertNull(dec1.source);
-	}*/
-	
-	@Test
-	public void testcase10() throws SyntaxException, LexicalException {
-		String input = "prog @expr k=12;"; 
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		Program ast=parser.program();  //Parse the program
-		show(ast);
-		assertEquals(ast.name,"prog");
-		assertEquals(ast.decsAndStatements.size(),0);
-	}
-	
-	@Test
-	public void testcase10parse() throws SyntaxException, LexicalException {
-		String input = "prog @expr k=12;"; 
+		String input = "prog k[[x,y]]"; 
 		show(input);
 		Scanner scanner = new Scanner(input).scan();  
 		show(scanner);   
@@ -263,21 +102,25 @@ public class ParserTest {
 	}
 	
 	@Test
-	public void testcase11() throws SyntaxException, LexicalException {
-		String input = "prog \"abcded\" boolean a=true;"; 
+	public void testcase2() throws SyntaxException, LexicalException {
+		String input = "p image1[x,y] = Z;"; 
 		show(input);
 		Scanner scanner = new Scanner(input).scan();  
 		show(scanner);   
 		Parser parser = new Parser(scanner);
-		Program ast=parser.program();  //Parse the program
-		show(ast);
-		assertEquals(ast.name,"prog");
-		assertEquals(ast.decsAndStatements.size(),0);
+		thrown.expect(SyntaxException.class);
+		try {
+			ASTNode ast=parser.parse();  //Parse the program
+		}
+		catch (SyntaxException e) {
+			show(e);
+			throw e;
+		} 
 	}
 	
 	@Test
-	public void testcase11_parse() throws SyntaxException, LexicalException {
-		String input = "prog \"abcded\" boolean a=true;"; 
+	public void testcase3() throws SyntaxException, LexicalException {
+		String input = "prog ident1 <-@7"; 
 		show(input);
 		Scanner scanner = new Scanner(input).scan();  
 		show(scanner);   
@@ -293,222 +136,11 @@ public class ParserTest {
 	}
 	
 
-	@Test
-	public void testcase12() throws SyntaxException, LexicalException {
-		String input = "isBoolean boolean ab=true; boolean cd==true; abcd=true ? return true: return false;"; //Should fail for ==
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			Program ast=parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		} 
-	}
 	
-	@Test
-	public void testcase13() throws SyntaxException, LexicalException {
-		String input = "isBoolean boolean ab=true; boolean cd==true; abcd=true ? return true: return false;"; //Should fail for =
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			Program ast=parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		} 
-	}
+
+
 	
-	@Test
-	public void testcase14() throws SyntaxException, LexicalException {
-		String input = "isUrl url filepng=\"abcd\"; \n @expr=12; url awesome=@expr; \n url filepng=abcdefg"; 
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		Program ast=parser.program();  //Parse the program
-		show(ast);
-		assertEquals(ast.name,"isUrl");
-		assertEquals(ast.decsAndStatements.size(),1);
-		Declaration_SourceSink dss=(Declaration_SourceSink)ast.decsAndStatements.get(0);
-		assertEquals(dss.name,"filepng");
-		assertEquals(dss.type,KW_url);
-		Source_StringLiteral s=(Source_StringLiteral)dss.source;
-		assertEquals(s.fileOrUrl,"abcd");
-	}
-	
-	@Test
-	public void testcase14_parse() throws SyntaxException, LexicalException {
-		String input = "isUrl url filepng=\"abcd\"; \n @expr=12; url awesome=@expr; \n url filepng=abcdefg"; 
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			ASTNode ast=parser.parse();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		}  
-	}
-	
-	@Test
-	public void testcase15() throws SyntaxException, LexicalException {
-		String input = "isUrl url filepng=\"abcd\" \n @expr=12; url awesome=@expr; \n url filepng=abcdefg"; //Should fail for ; in line one
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			Program ast=parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		}  
-	}
-	
-	@Test
-	public void testcase16() throws SyntaxException, LexicalException {
-		String input = "isFile file filepng=\"abcd\"; \n @expr=12; url filepng=@expr; \n url filepng=abcdefg"; 
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		Program ast=parser.program();  //Parse the program
-		show(ast);
-		assertEquals(ast.name,"isFile");
-		assertEquals(ast.decsAndStatements.size(),1);
-		assertEquals(ast.firstToken.kind,IDENTIFIER);
-		
-		// Declaration Statements
-		Declaration_SourceSink ds=(Declaration_SourceSink)ast.decsAndStatements.get(0);
-		assertEquals(ds.type,KW_file);
-		assertEquals(ds.name,"filepng");
-		Source_StringLiteral s=(Source_StringLiteral)ds.source;
-		assertEquals(s.fileOrUrl,"abcd");
-		//assertEquals(ast.)
-	}
-	
-	@Test
-	public void testcase16_parse() throws SyntaxException, LexicalException {
-		String input = "isFile file filepng=\"abcd\"; \n @expr=12; url filepng=@expr; \n url filepng=abcdefg"; 
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			ASTNode ast=parser.parse();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		}  
-	}
-	
-	@Test
-	public void testcase17() throws SyntaxException, LexicalException {
-		String input =  "isFile file filepng=\"abcd\" \n @expr=12; url filepng=@expr; \n url filepng=abcdefg";  //Should fail for ; in line one
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			Program ast=parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		}  
-	}
-	
-	@Test
-	public void testcase18() throws SyntaxException, LexicalException {
-		String input =  "isurl url urlname;";  //Should fail for url as url can only be initalised
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		thrown.expect(SyntaxException.class);
-		try {
-			Program ast=parser.program();  //Parse the program
-		}
-		catch (SyntaxException e) {
-			show(e);
-			throw e;
-		}  
-	}
-	
-	@Test
-	public void testcase19() throws SyntaxException, LexicalException {
-		String input =  "declaration int xyz;\n boolean zya;\n image imagename;";  
-		show(input);
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		Program ast=parser.program();  //Parse the program
-		show(ast);
-		assertEquals(ast.name,"declaration");
-		assertEquals(ast.firstToken.kind,IDENTIFIER);
-		
-		//Declaration statements start
-		Declaration_Variable dv1=(Declaration_Variable)ast.decsAndStatements.get(0);
-		assertEquals(dv1.name,"xyz");
-		assertEquals(dv1.type.kind,KW_int);
-		assertNull(dv1.e);
-		
-		Declaration_Variable dv2=(Declaration_Variable)ast.decsAndStatements.get(1);
-		assertEquals(dv2.name,"zya");
-		assertEquals(dv2.type.kind,KW_boolean);
-		assertNull(dv2.e);
-		
-		Declaration_Image dv3=(Declaration_Image)ast.decsAndStatements.get(2);	
-		assertEquals(dv3.name,"imagename");
-		assertNull(dv3.source);
-		assertNull(dv3.xSize);
-		assertNull(dv3.ySize);
-		
-		//Declaration statement end
-	}
-	
-	@Test
-	public void testcase20() throws SyntaxException, LexicalException {
-		String input =  "imageProgram image imageName;"
-				+ "\n imageName->abcdpng; "
-				+ "\n imageName -> SCREEN; "
-				+ "\n imageName <- \"awesome\";"
-				+ "\n imageName <- @express; \n"
-				+ "\n imageName <- abcdpng;";  // Image related Test cases
-		Scanner scanner = new Scanner(input).scan();  
-		show(scanner);   
-		Parser parser = new Parser(scanner);
-		Program ast=parser.program();  //Parse the program
-		show(ast);
-		assertEquals(ast.name,"imageProgram");
-		
-		//Declaration statement start
-		Declaration_Image dv1=(Declaration_Image)ast.decsAndStatements.get(0);
-		assertEquals(dv1.name,"imageName");
-		assertNull(dv1.xSize);
-		assertNull(dv1.ySize);
-		assertNull(dv1.source);
-		
-		Statement_Out dv2=(Statement_Out)ast.decsAndStatements.get(1);
-		assertEquals(dv2.name,"imageName");
-		Sink_Ident si2=(Sink_Ident)dv2.sink;
-		assertEquals(si2.name,"abcdpng");
+
+
 }
-}
+
